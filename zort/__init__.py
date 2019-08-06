eebff3ff848ac469af04e45592709c9bbbcb85d8
Michael Medford <medford2@llnl.gov>
2019-08-06 10:57:54 -0700

    package can be installed from setup.py

--- zort/__init__.py
+++ zort/__init__.py
@@ -3,7 +3,10 @@
 __init__.py
 """
 
-__version__ = open('../VERSION').readline().strip()
+from pathlib import Path
+
+parentDir = Path(__file__).parent.parent
+__version__ = open('%s/VERSION' % parentDir).readline().strip()
 
 # The first time this script is called, you will be asked for the data directory of your ZTF lightcurves, as well
 # as the opportunity to build (or download) object files and rcid_map files if they are not detected.
