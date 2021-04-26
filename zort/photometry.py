aad6d0c3518e2491ba490dff64dd386f0d457f4b
Michael Medford <MichaelMedford@berkeley.edu>
2021-04-26 12:44:26 -0400

    make magnitude and flux errors optional

--- zort/photometry.py
+++ zort/photometry.py
@@ -7,13 +7,19 @@ A set of functions for performing photometric calculations on lightcurves.
 import numpy as np
 
 
-def magnitudes_to_fluxes(mag, magerr, zero_point=22.0):
+def magnitudes_to_fluxes(mag, magerr=None, zero_point=22.0):
     flux = 10 ** ((zero_point - mag) / 2.5)
-    fluxerr = flux * magerr / 1.086
+    if magerr:
+        fluxerr = flux * magerr / 1.086
+    else:
+        fluxerr = None
     return flux, fluxerr
 
 
-def fluxes_to_magnitudes(flux, fluxerr, zero_point=22.0):
+def fluxes_to_magnitudes(flux, fluxerr=None, zero_point=22.0):
     mag = zero_point - 2.5 * np.log10(flux)
-    magerr = 1.086 * fluxerr / flux
+    if fluxerr:
+        magerr = 1.086 * fluxerr / flux
+    else:
+        magerr = None
     return mag, magerr
